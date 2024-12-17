320f19fa754a88ecca276f6b49f1d0f8e095b13e
GiuntaGiovanni <jhonniest741@gmail.com>
2024-12-17 17:08:10 +0100

    [feat] logica per generare il prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -11,30 +11,59 @@ Il programma dovrà chiedere all’utente il numero di chilometri che vuole perc
 ************************/
 
 
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero. 
 
 // Input
-// richiesta all'utente del numero di chilometri che vuole percorrere
-// richiesta all'utente dell'età del passeggero
+// Chiediamo all'utente il numero di chilometri che vuole percorrere
+let chilometriuser = parseInt(prompt("Quanti chilometri devi percorrere?"))
+
+// Chiediamo all'utente l'età del passeggero
+let etauser = parseInt(prompt("Inserisci l'età del passeggero"))
+
+console.log("il passeggero deve percorrere", chilometriuser, "km");
+console.log("Il passeggero ha", etauser, "anni");
+
+
 
 // Elaborazione
-// Calcolare il prezzo del biglietto in base ai km (0.21 € al km)
+// Calcoliamo il prezzo del biglietto in base ai km (0.21 € al km)
+
     // moltiplico i chilometri inseriti dall'utente con il prezzo al km
-    // mostro il prezzo della tratta
+    let prezzotratta = chilometriuser * 0.21;
+    // mostro il prezzo base della tratta
+    console.log("Il prezzo base è di", prezzotratta.toFixed(2),"€");
+    
+// verifico il risultato salvandomi il dato
+let risultato
 
 // se l'eta del passeggero è minore di 18 applicare uno sconto del 20%
-    // (moltiplico il prezzo della tratta a 0,20 e il risultato) e lo sottraggo al prezzo tratta
-    // mostro il risultato del prezzo tratta minori
+if (etauser < 18) {
 
-// altrimenti se l'eta del passeggero è maggiore di 65 applicare uno sconto del 40%
-    // (moltiplico il prezzo della tratta a 0,40 ) e lo sottraggo al prezzo tratta
-    // mostro il risultato del prezzo tratta senior
+    // sconto del 20%
+    risultato = prezzotratta * 0.80 
+
+console.log("Congratulazioni!...hai uno sconto del 20%");
+    
+    // altrimenti se l'eta del passeggero è maggiore di 65 applicare uno sconto del 40%   
+} else if (etauser > 65) {
+    
+    // sconto del 40%
+    risultato = prezzotratta * 0.60
+
+    console.log("Congratulazioni!...hai uno sconto del 40%");
 
 // altrimenti prezzo standard
-    // mostro il prezzo della tratta
+}else {
 
+    // prezzo base
+    risultato = prezzotratta;
+
+    console.log("Mi dispiace, ma non sei nè troppo grande e nè troppo piccolo quindi non ci sono sconti applicabili");
+
+
+}
 
 // output
-// mostriamo all'utente quuanto deve pagare considerando i centesimi di euro
+// mostriamo all'utente quanto deve pagare considerando i centesimi di euro
+console.log("Il passeggero deve pagare:", risultato.toFixed(2),"€");
 
 
